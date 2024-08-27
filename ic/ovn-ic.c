simonartxavier/ovn

commit 37fd1dd378eab04ec67030472bf4f3bbc48242ef

ovn-ic: Handle NB:name updates properly.
When the user updates the NB_GLOBAL.name after registering
to IC Databases if the user already has defined chassis as a gateway
that will cause ovn-ic instance to run in an infinity loop trying
to update the gateways and insert the current gateway to the SB.chassis
tables as a remote chassis (we match on the new AZ ) which will fail since
we already have this chassis with is-interconn in local SB.

This patch aims to fix the above issues by updating the AZ.name only
when the user updates the NB.name locally.

Signed-off-by: Mohammad Heib <mheib@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

ic/ovn-ic.c
@@ -132,14 +132,18 @@ az_run(struct ic_context *ctx)
         return NULL;
     }
 
-    /* Delete old AZ if name changes.  Note: if name changed when ovn-ic
-     * is not running, one has to manually delete the old AZ with:
+    /* Update old AZ if name changes.  Note: if name changed when ovn-ic
+     * is not running, one has to manually delete/update the old AZ with:
      * "ovn-ic-sbctl destroy avail <az>". */
     static char *az_name;
     const struct icsbrec_availability_zone *az;
     if (az_name && strcmp(az_name, nb_global->name)) {
         ICSBREC_AVAILABILITY_ZONE_FOR_EACH (az, ctx->ovnisb_idl) {
-            if (!strcmp(az->name, az_name)) {
+            /* AZ name update locally need to update az in ISB. */
+            if (nb_global->name[0] && !strcmp(az->name, az_name)) {
+                icsbrec_availability_zone_set_name(az, nb_global->name);
+                break;
+            } else if (!nb_global->name[0] && !strcmp(az->name, az_name)) {
                 icsbrec_availability_zone_delete(az);
                 break;
             }

tests/ovn-ic.at
@@ -28,7 +28,31 @@ availability-zone az3
 ])
 
 OVN_CLEANUP_IC([az1], [az2])
+AT_CLEANUP
+])
 
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([ovn-ic -- AZ update in GW])
+ovn_init_ic_db
+net_add n1
+
+ovn_start az1
+sim_add gw-az1
+as gw-az1
+
+check ovs-vsctl add-br br-phys
+ovn_az_attach az1 n1 br-phys 192.168.1.1
+check ovs-vsctl set open . external-ids:ovn-is-interconn=true
+
+az_uuid=$(fetch_column ic-sb:availability-zone _uuid name="az1")
+ovn_as az1 ovn-nbctl set NB_Global . name="az2"
+wait_column "$az_uuid" ic-sb:availability-zone _uuid name="az2"
+
+# make sure that gateway still point to the same AZ with new name
+wait_column "$az_uuid" ic-sb:gateway availability_zone name="gw-az1"
+
+OVN_CLEANUP_IC([az1])
 AT_CLEANUP
 ])
 

commit 8f6165a0e7cadcb97289b72b3cc73effc3616c08

ovn-ic: Fix potential segmentation violation.
ERROR: AddressSanitizer: SEGV on unknown address 0x000000000020 (pc 0x000000709f68 bp 0x7ffe8546c9b0 sp 0x7ffe8546c9b0 T0)
  0 0x709f68 in hmap_insert_fast ./ovs/./include/openvswitch/hmap.h
  1 0x702611 in hmap_insert_at ./ovs/./include/openvswitch/hmap.h:307:5
  2 0x70444c in ovsdb_idl_txn_write__ ./ovs/lib/ovsdb-idl.c:3631:9
  3 0x704141 in ovsdb_idl_txn_write ./ovs/lib/ovsdb-idl.c:3688:5
  4 0x6c1cd1 in icsbrec_availability_zone_set_name ./lib/ovn-ic-sb-idl.c:395:5
  5 0x5a7f1e in az_run ./ic/ovn-ic.c:144:17
  6 0x5a7f1e in main ./ic/ovn-ic.c:2323:62
  7 0x7fc605b6db74 in __libc_start_main (/lib64/libc.so.6+0x27b74)
  8 0x4c8fed in _start (/root/master-ovn/ic/ovn-ic+0x4c8fed)

Fixes: 37fd1dd378ea ("ovn-ic: Handle NB:name updates properly.")

Signed-off-by: Xavier Simonart <xsimonar@redhat.com>

ic/ovn-ic.c
@@ -137,19 +137,21 @@ az_run(struct ic_context *ctx)
      * "ovn-ic-sbctl destroy avail <az>". */
     static char *az_name;
     const struct icsbrec_availability_zone *az;
-    if (az_name && strcmp(az_name, nb_global->name)) {
-        ICSBREC_AVAILABILITY_ZONE_FOR_EACH (az, ctx->ovnisb_idl) {
-            /* AZ name update locally need to update az in ISB. */
-            if (nb_global->name[0] && !strcmp(az->name, az_name)) {
-                icsbrec_availability_zone_set_name(az, nb_global->name);
-                break;
-            } else if (!nb_global->name[0] && !strcmp(az->name, az_name)) {
-                icsbrec_availability_zone_delete(az);
-                break;
+    if (ctx->ovnisb_txn) {
+        if (az_name && strcmp(az_name, nb_global->name)) {
+            ICSBREC_AVAILABILITY_ZONE_FOR_EACH (az, ctx->ovnisb_idl) {
+                /* AZ name update locally need to update az in ISB. */
+                if (nb_global->name[0] && !strcmp(az->name, az_name)) {
+                    icsbrec_availability_zone_set_name(az, nb_global->name);
+                    break;
+                } else if (!nb_global->name[0] && !strcmp(az->name, az_name)) {
+                    icsbrec_availability_zone_delete(az);
+                    break;
+                }
             }
+            free(az_name);
+            az_name = NULL;
         }
-        free(az_name);
-        az_name = NULL;
     }
 
     if (!nb_global->name[0]) {