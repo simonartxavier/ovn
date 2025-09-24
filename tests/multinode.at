simonartxavier/ovn

commit 987adc9e589ea9c5f98c91bf56fc704ac6beed6a

controller: Avoid IPv6 Mac_Binding related transaction errors.
When using Distributed Gateway Port and HA, IPv6 ND_NA packet
received on the router port causes all HA chassis to try updating the
Mac_Binding table, often causing transaction errors.
This patch prevents this by avoiding Mac_Binding updates due to ND_NS or NA
packets received on non residend DGP.

Modified test "4 HV, 3 LS, 2 LR, packet test with HA distributed router gateway port"
to often create such behavior.

While at modifying the test, also fix some potential flakes.

Signed-off-by: Xavier Simonart <xsimonar@redhat.com>
Reported-at: https://issues.redhat.com/browse/FDP-1567
Acked-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Ales Musil <amusil@redhat.com>

northd/northd.c
@@ -13742,6 +13742,19 @@ build_neigh_learning_flows_for_lrouter_port(
                                 &op->nbrp->header_,
                                 lflow_ref);
     }
+
+    if (lrp_is_l3dgw(op)) {
+        ds_clear(match);
+        ds_put_format(match, "inport == %s && (nd_na || nd_ns) && "
+                             "!is_chassis_resident(%s)", op->json_key,
+                             op->cr_port->json_key);
+        ovn_lflow_add_with_hint(lflows, op->od,
+                                S_ROUTER_IN_LOOKUP_NEIGHBOR, 120,
+                                ds_cstr(match),
+                                REGBIT_LOOKUP_NEIGHBOR_RESULT" = 1; next;",
+                                &op->nbrp->header_,
+                                lflow_ref);
+    }
 }
 
 /* Logical router ingress table ND_RA_OPTIONS & ND_RA_RESPONSE: IPv6 Router

tests/ovn-northd.at
@@ -7278,6 +7278,9 @@ AT_CHECK([grep lr_in_lookup_neighbor lrflows | grep cr-DR | ovn_strip_lflows], [
   table=??(lr_in_lookup_neighbor), priority=100  , match=(inport == "DR-S1" && arp.spa == 172.16.1.0/24 && arp.op == 1 && is_chassis_resident("cr-DR-S1")), action=(reg9[[2]] = lookup_arp(inport, arp.spa, arp.sha); next;)
   table=??(lr_in_lookup_neighbor), priority=100  , match=(inport == "DR-S2" && arp.spa == 172.16.2.0/24 && arp.op == 1 && is_chassis_resident("cr-DR-S2")), action=(reg9[[2]] = lookup_arp(inport, arp.spa, arp.sha); next;)
   table=??(lr_in_lookup_neighbor), priority=100  , match=(inport == "DR-S3" && arp.spa == 172.16.3.0/24 && arp.op == 1 && is_chassis_resident("cr-DR-S3")), action=(reg9[[2]] = lookup_arp(inport, arp.spa, arp.sha); next;)
+  table=??(lr_in_lookup_neighbor), priority=120  , match=(inport == "DR-S1" && (nd_na || nd_ns) && !is_chassis_resident("cr-DR-S1")), action=(reg9[[2]] = 1; next;)
+  table=??(lr_in_lookup_neighbor), priority=120  , match=(inport == "DR-S2" && (nd_na || nd_ns) && !is_chassis_resident("cr-DR-S2")), action=(reg9[[2]] = 1; next;)
+  table=??(lr_in_lookup_neighbor), priority=120  , match=(inport == "DR-S3" && (nd_na || nd_ns) && !is_chassis_resident("cr-DR-S3")), action=(reg9[[2]] = 1; next;)
 ])
 # Check the flows in lr_in_gw_redirect stage
 AT_CHECK([grep lr_in_gw_redirect lrflows | grep cr-DR | ovn_strip_lflows], [0], [dnl

tests/ovn.at
@@ -12471,11 +12471,19 @@ sim_add gw1
 as gw1
 check ovs-vsctl add-br br-phys
 ovn_attach n1 br-phys 192.168.0.2
+check ovs-vsctl set Open_vSwitch . external-ids:garp-max-timeout-sec=2
 
 sim_add gw2
 as gw2
 check ovs-vsctl add-br br-phys
 ovn_attach n1 br-phys 192.168.0.4
+check ovs-vsctl set Open_vSwitch . external-ids:garp-max-timeout-sec=2
+
+sim_add gw3
+as gw3
+check ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.6
+check ovs-vsctl set Open_vSwitch . external-ids:garp-max-timeout-sec=2
 
 sim_add ext1
 as ext1
@@ -12526,7 +12534,7 @@ check ovn-nbctl lr-route-add R0 0.0.0.0/0 100.60.1.2
 check ovn-nbctl lr-route-add R1 192.168.0.0/16 100.60.1.1
 
 # Connect alice to R1 as distributed router gateway port on gw1
-check ovn-nbctl lrp-add R1 alice 00:00:02:01:02:03 172.16.1.1/24
+check ovn-nbctl lrp-add R1 alice 00:00:02:01:02:03 172.16.1.1/24 fd10::1/64
 
 AT_CHECK([ovn-nbctl \
     --id=@gc0 create Gateway_Chassis name=alice_gw1 \
@@ -12535,8 +12543,12 @@ AT_CHECK([ovn-nbctl \
     --id=@gc1 create Gateway_Chassis name=alice_gw2 \
                                      chassis_name=gw2 \
                                      priority=10 -- \
-    set Logical_Router_Port alice 'gateway_chassis=[@gc0,@gc1]' | uuidfilt], [0], [<0>
+    --id=@gc2 create Gateway_Chassis name=alice_gw3 \
+                                     chassis_name=gw3 \
+                                     priority=5 -- \
+    set Logical_Router_Port alice 'gateway_chassis=[@gc0,@gc1,@gc2]' | uuidfilt], [0], [<0>
 <1>
+<2>
 ])
 
 check ovn-nbctl lsp-add alice rp-alice -- set Logical_Switch_Port rp-alice \
@@ -12549,7 +12561,7 @@ check ovn-nbctl lsp-add foo foo1 \
 
 # Create logical port outside1 in outside
 check ovn-nbctl lsp-add outside outside1 \
--- lsp-set-addresses outside1 "f0:00:00:01:02:04 172.16.1.3"
+-- lsp-set-addresses outside1 unknown
 
 # Create localnet port in alice
 check ovn-nbctl lsp-add alice ln-alice
@@ -12567,11 +12579,12 @@ check ovn-nbctl lsp-set-options ln-outside network_name=phys
 # mapping to the external network, is the one generating packets
 check as gw1 ovs-vsctl set open . external-ids:ovn-bridge-mappings=phys:br-phys
 check as gw2 ovs-vsctl set open . external-ids:ovn-bridge-mappings=phys:br-phys
+check as gw3 ovs-vsctl set open . external-ids:ovn-bridge-mappings=phys:br-phys
 check as ext1 ovs-vsctl set open . external-ids:ovn-bridge-mappings=phys:br-phys
 
 wait_for_ports_up
 check ovn-nbctl --wait=sb sync
-OVN_WAIT_PATCH_PORT_FLOWS(["ln-alice"], ["gw1"] ["gw2"])
+OVN_WAIT_PATCH_PORT_FLOWS(["ln-alice"], ["gw1"] ["gw2"],["gw3"])
 OVN_WAIT_PATCH_PORT_FLOWS(["ln-outside"], ["ext1"])
 
 ovn-sbctl dump-flows > sbflows
@@ -12588,15 +12601,23 @@ check ovn-nbctl --wait=hv sync
 
 hv1_gw1_ofport=$(as hv1 ovs-vsctl --bare --columns ofport find Interface name=ovn-gw1-0)
 hv1_gw2_ofport=$(as hv1 ovs-vsctl --bare --columns ofport find Interface name=ovn-gw2-0)
+hv1_gw3_ofport=$(as hv1 ovs-vsctl --bare --columns ofport find Interface name=ovn-gw3-0)
 
 OVS_WAIT_UNTIL([
-    test 1 = $(as hv1 ovs-ofctl dump-flows br-int table=OFTABLE_REMOTE_OUTPUT | grep -c "active_backup,ofport,members:$hv1_gw1_ofport,$hv1_gw2_ofport")
+    test 1 = $(as hv1 ovs-ofctl dump-flows br-int table=OFTABLE_REMOTE_OUTPUT | grep -c "active_backup,ofport,members:$hv1_gw1_ofport,$hv1_gw2_ofport,$hv1_gw3_ofport")
 ])
 
+n_external_ip=50
 test_ip_packet()
 {
     local active_gw=$1
     local backup_gw=$2
+
+    # Send many GARP/ND_NA from outside1 to the router.
+    for i in $(seq 11 $((n_external_ip + 10))); do
+        send_garp ext1 ext1-vif1 1 "00:00:00:00:20:$i" "ff:ff:ff:ff:ff:ff" 172.16.1.$i 172.16.1.$i
+        send_na ext1 ext1-vif1 "00:00:00:00:30:$i" "33:33:00:00:00:01" "fd10::$i" "ff02::1"
+    done
 
     # Send ip packet between foo1 and outside1
     src_mac="f00000010203" # foo1 mac
@@ -12633,6 +12654,10 @@ grep actions=mod_dl_dst:f0:00:00:01:02:04 | wc -l` -eq 1
     dst_ip=`ip_to_hex 172 16 1 3`
     expected=${dst_mac}${src_mac}08004500001c000000003e110200${src_ip}${dst_ip}0035111100080000
     echo $expected > ext1-vif1.expected
+
+    # Wait for first packet to be received before resetting pcap on ext1
+    OVN_CHECK_PACKETS_CONTAIN([ext1/vif1-tx.pcap], [ext1-vif1.expected])
+
     exp_gw_ip_garp=ffffffffffff00000201020308060001080006040001000002010203ac100101000000000000ac100101
     echo $exp_gw_ip_garp >> ext1-vif1.expected
 
@@ -12665,15 +12690,28 @@ AT_CHECK([ovn-nbctl --wait=hv \
     --id=@gc1 create Gateway_Chassis name=alice_gw2 \
                                      chassis_name=gw2 \
                                      priority=20 -- \
-    set Logical_Router_Port alice 'gateway_chassis=[@gc0,@gc1]' | uuidfilt], [0], [<0>
+    --id=@gc2 create Gateway_Chassis name=alice_gw3 \
+                                     chassis_name=gw3 \
+                                     priority=5 -- \
+    set Logical_Router_Port alice 'gateway_chassis=[@gc0,@gc1,@gc2]' | uuidfilt], [0], [<0>
 <1>
+<2>
 ])
 
 OVS_WAIT_UNTIL([
-    test 1 = $(as hv1 ovs-ofctl dump-flows br-int table=OFTABLE_REMOTE_OUTPUT | grep -c "active_backup,ofport,members:$hv1_gw2_ofport,$hv1_gw1_ofport")
+    test 1 = $(as hv1 ovs-ofctl dump-flows br-int table=OFTABLE_REMOTE_OUTPUT | grep -c "active_backup,ofport,members:$hv1_gw2_ofport,$hv1_gw1_ofport,$hv1_gw3_ofport")
 ])
 
 test_ip_packet gw2 gw1
+
+# n_external_ip * 2 + **1** as 172.16.1.3.
+# 172.16.1.1 (i.e. the router port) might sometimes be seen as a mac_binding. Ignore it.
+wait_row_count MAC_Binding $((n_external_ip * 2 + 1)) ip!=172.16.1.1
+
+for i in gw1 gw2 gw3; do
+    AT_CHECK([grep -c "Transaction causes multiple rows" $i/ovn-controller.log], [1], [0
+])
+done
 
 OVN_CLEANUP([hv1],[gw1],[gw2],[ext1])
 AT_CLEANUP

commit 46b9541195c974c98b8cadf42b8780dfca2151bd

northd: Fix receiving NA on non resident chassis.
Since [0], ND/NA received on non-resident chassis are dropped/ignored
on DGP to avoid having all chassis of an HA group trying to update
mac_binding, which was resulting in transaction errors.
However, it can happen that the whole pipeline, including the distributed
gateway router, is run on a chassis where the DGP is not resident.
This happens for instance in the following setup, with vif1 and ext on hv1
and lr-pub on hv2.
vif1 --- ls1 --- lr --- pub -+- ln
                             +- ext

In this setup, unicast NA from ext1 towards vif1 should not be ignored,
even on the non-resident chassis.

This patch amends [0] to only ignore multicast ND/NA on non-resident chassis.
[0]: 987adc9e589e ("controller: Avoid IPv6 Mac_Binding related transaction errors.")

Fixes: 987adc9e589e ("controller: Avoid IPv6 Mac_Binding related transaction errors.")

Signed-off-by: Xavier Simonart <xsimonar@redhat.com>
Reported-at: https://issues.redhat.com/browse/FDP-1728

northd/northd.c
@@ -13763,6 +13763,7 @@ build_neigh_learning_flows_for_lrouter_port(
     if (lrp_is_l3dgw(op)) {
         ds_clear(match);
         ds_put_format(match, "inport == %s && (nd_na || nd_ns) && "
+                             "eth.mcast && "
                              "!is_chassis_resident(%s)", op->json_key,
                              op->cr_port->json_key);
         ovn_lflow_add_with_hint(lflows, op->od,

tests/multinode-macros.at
@@ -313,7 +313,7 @@ m_check_column() {
 # interface in the NETNS namespace.  It also configures it with the provided
 # IP and (optionally) default gateway GW.
 m_add_internal_port() {
-    local node=$1 ns=$2 br=$3 port=$4 ip=$5 gw=$6
+    local node=$1 ns=$2 br=$3 port=$4 ip=$5 gw=$6 ip6=$7
 
     if ! m_as $node ip netns list | grep $ns; then
         check m_as $node ip netns add $ns
@@ -327,6 +327,9 @@ m_add_internal_port() {
 
     if test -n "$gw"; then
         check m_as $node ip netns exec $ns ip route add default via $gw dev $port
+    fi
+    if test -n "$ip6"; then
+        check m_as $node ip netns exec $ns ip addr add $ip6 dev $port
     fi
 }
 

tests/multinode.at
@@ -3834,3 +3834,133 @@ M_NS_CHECK_EXEC([ovn-chassis-1], [vm1], [ping -q -c 3 -i 0.3 -w 2 20.0.0.2 | FOR
 ])
 
 AT_CLEANUP
+
+AT_SETUP([IPv6 NA received on non resident chassis])
+m_as ovn-chassis-1 ovs-vsctl set open . external-ids:ovn-bridge-mappings=public:br-ex
+m_as ovn-chassis-2 ovs-vsctl set open . external-ids:ovn-bridge-mappings=public:br-ex
+
+check_fake_multinode_setup
+
+# Delete the multinode NB and OVS resources before starting the test.
+cleanup_multinode_resources
+
+m_as ovn-chassis-1 ip link del ls1p1-p
+m_as ovn-chassis-1 ip link del ls1p2-p
+m_as ovn-chassis-1 ip link del ls2p1-p
+
+OVS_WAIT_UNTIL([m_as ovn-chassis-1 ip link show | grep -q genev_sys])
+OVS_WAIT_UNTIL([m_as ovn-chassis-2 ip link show | grep -q genev_sys])
+
+check multinode_nbctl ls-add ls1
+check multinode_nbctl lsp-add ls1 ls1p1
+check multinode_nbctl lsp-set-addresses ls1p1 "00:00:00:01:01:02 192.168.1.1 2001::1"
+check multinode_nbctl lsp-add ls1 ls1p2
+check multinode_nbctl lsp-set-addresses ls1p2 "00:00:00:01:02:02 192.168.1.2 2001::2"
+check multinode_nbctl lr-add lr1
+check multinode_nbctl lrp-add lr1 lr1-ls1 00:00:00:00:00:01 192.168.1.254/24 2001::a/64
+check multinode_nbctl lsp-add ls1 ls1-lr1
+check multinode_nbctl lsp-set-addresses ls1-lr1 "00:00:00:00:00:01 192.168.1.254 2001::a"
+check multinode_nbctl lsp-set-type ls1-lr1 router
+check multinode_nbctl lsp-set-options ls1-lr1 router-port=lr1-ls1
+
+check multinode_nbctl lrp-add lr1 lr1-ls2 00:00:00:00:00:02 192.168.2.254/24 2002::a/64
+check multinode_nbctl ls-add ls2
+check multinode_nbctl lsp-add ls2 ls2-lr1
+check multinode_nbctl lsp-set-addresses ls2-lr1 "00:00:00:00:00:02 192.168.2.254 2002::a"
+check multinode_nbctl lsp-set-type ls2-lr1 router
+check multinode_nbctl lsp-set-options ls2-lr1 router-port=lr1-ls2
+check multinode_nbctl lsp-add ls2 ls2p1
+check multinode_nbctl lsp-set-addresses ls2p1 "00:00:00:02:01:02 192.168.2.1 2002::1"
+
+check multinode_nbctl lsp-add ls1 ls1p3
+check multinode_nbctl lsp-set-addresses ls1p3 "00:00:00:01:03:02 192.168.1.3 2001::3"
+check multinode_nbctl lrp-add lr1 lr1-pub 0a:0a:56:33:02:ff 172.18.86.254/24 6812:86::254/64
+check multinode_nbctl ls-add pub                    \
+    -- lsp-add pub pub-lr1                          \
+    -- lsp-set-type pub-lr1 router                  \
+    -- lsp-set-addresses pub-lr1 router             \
+    -- lsp-set-options pub-lr1 router-port=lr1-pub  \
+    -- lsp-add pub pub-ln                           \
+    -- lsp-set-type pub-ln localnet                 \
+    -- lsp-set-addresses pub-ln unknown             \
+    -- lsp-set-options pub-ln network_name=public
+
+check multinode_nbctl lrp-set-gateway-chassis lr1-pub ovn-chassis-2
+
+check multinode_nbctl lr-nat-add lr1 dnat_and_snat 172.18.86.11 192.168.1.1 ls1p1 0a:0a:56:33:02:11
+check multinode_nbctl lr-nat-add lr1 dnat_and_snat 6812:86::11 2001::1 ls1p1 0a:0a:56:33:02:11
+
+m_as ovn-chassis-1 /data/create_fake_vm.sh ls1p1 ls1p1 00:00:00:01:01:02 1500 192.168.1.1 24 192.168.1.254 2001::1/64 2001::a
+m_as ovn-chassis-1 /data/create_fake_vm.sh ls1p2 ls1p2 00:00:00:01:02:02 1500 192.168.1.2 24 192.168.1.254 2001::2/64 2001::a
+m_as ovn-chassis-1 /data/create_fake_vm.sh ls2p1 ls2p1 00:00:00:02:01:02 1500 192.168.2.1 24 192.168.2.254 2002::2/64 2002::a
+m_add_internal_port ovn-chassis-1 ovn-ext1 br-ex ext1 172.18.86.101/24 "" 6812:86::101/64
+m_add_internal_port ovn-chassis-2 ovn-ext2 br-ex ext2 172.18.86.102/24 "" 6812:86::102/64
+
+m_central_as ovn-sbctl --all destroy  mac_binding
+
+M_NS_CHECK_EXEC([ovn-chassis-1], [ls1p1], [ping -q -c 3 -i 0.3 -w 2 172.18.86.101 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+m_wait_row_count mac_binding 1 ip="172.18.86.101" logical_port="lr1-pub"
+
+M_NS_CHECK_EXEC([ovn-chassis-1], [ls1p1], [ping -q -c 3 -i 0.3 -w 2 172.18.86.102 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+m_wait_row_count mac_binding 1 ip="172.18.86.102" logical_port="lr1-pub"
+
+M_NS_CHECK_EXEC([ovn-chassis-1], [ls1p1], [ping6 -q -c 3 -i 0.3 -w 2 6812:86::101 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+m_wait_row_count mac_binding 1 ip="6812\:86\:\:101" logical_port="lr1-pub"
+
+M_NS_CHECK_EXEC([ovn-chassis-1], [ls1p1], [ping6 -q -c 3 -i 0.3 -w 2 6812:86::102 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+m_wait_row_count mac_binding 1 ip="6812\:86\:\:102" logical_port="lr1-pub"
+
+# We remove all mac_bindings to check that neighbor learning works also in the other direction.
+m_central_as ovn-sbctl --all destroy  mac_binding
+
+# Also remove addresses learned on ovn-ext
+M_NS_CHECK_EXEC([ovn-chassis-1], [ovn-ext1], [ip -6 neigh del 6812:86::11 dev ext1 lladdr 0a:0a:56:33:02:11])
+M_NS_CHECK_EXEC([ovn-chassis-2], [ovn-ext2], [ip -6 neigh del 6812:86::11 dev ext2 lladdr 0a:0a:56:33:02:11])
+
+M_NS_CHECK_EXEC([ovn-chassis-1], [ovn-ext1], [ping -q -c 3 -i 0.3 -w 2 172.18.86.11 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+m_wait_row_count mac_binding 1 ip="172.18.86.101" logical_port="lr1-pub"
+
+M_NS_CHECK_EXEC([ovn-chassis-2], [ovn-ext2], [ping -q -c 3 -i 0.3 -w 2 172.18.86.11 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+m_wait_row_count mac_binding 1 ip="172.18.86.101" logical_port="lr1-pub"
+
+M_NS_CHECK_EXEC([ovn-chassis-1], [ovn-ext1], [ping6 -q -c 3 -i 0.3 -w 2 6812:86::11 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+m_wait_row_count mac_binding 1 ip="6812\:86\:\:101" logical_port="lr1-pub"
+
+M_NS_CHECK_EXEC([ovn-chassis-2], [ovn-ext2], [ping6 -q -c 3 -i 0.3 -w 2 6812:86::11 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+m_wait_row_count mac_binding 1 ip="6812\:86\:\:102" logical_port="lr1-pub"
+
+# Ping from ext2 while no mac_binding yet.
+m_central_as ovn-sbctl --all destroy  mac_binding
+M_NS_CHECK_EXEC([ovn-chassis-2], [ovn-ext2], [ip -6 neigh del 6812:86::11 dev ext2 lladdr 0a:0a:56:33:02:11])
+
+M_NS_CHECK_EXEC([ovn-chassis-2], [ovn-ext2], [ping6 -q -c 3 -i 0.3 -w 2 6812:86::11 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+m_wait_row_count mac_binding 1 ip="6812\:86\:\:102" logical_port="lr1-pub"
+
+AT_CLEANUP

tests/ovn-northd.at
@@ -7311,9 +7311,9 @@ AT_CHECK([grep lr_in_lookup_neighbor lrflows | grep cr-DR | ovn_strip_lflows], [
   table=??(lr_in_lookup_neighbor), priority=100  , match=(inport == "DR-S1" && arp.spa == 172.16.1.0/24 && arp.op == 1 && is_chassis_resident("cr-DR-S1")), action=(reg9[[2]] = lookup_arp(inport, arp.spa, arp.sha); next;)
   table=??(lr_in_lookup_neighbor), priority=100  , match=(inport == "DR-S2" && arp.spa == 172.16.2.0/24 && arp.op == 1 && is_chassis_resident("cr-DR-S2")), action=(reg9[[2]] = lookup_arp(inport, arp.spa, arp.sha); next;)
   table=??(lr_in_lookup_neighbor), priority=100  , match=(inport == "DR-S3" && arp.spa == 172.16.3.0/24 && arp.op == 1 && is_chassis_resident("cr-DR-S3")), action=(reg9[[2]] = lookup_arp(inport, arp.spa, arp.sha); next;)
-  table=??(lr_in_lookup_neighbor), priority=120  , match=(inport == "DR-S1" && (nd_na || nd_ns) && !is_chassis_resident("cr-DR-S1")), action=(reg9[[2]] = 1; next;)
-  table=??(lr_in_lookup_neighbor), priority=120  , match=(inport == "DR-S2" && (nd_na || nd_ns) && !is_chassis_resident("cr-DR-S2")), action=(reg9[[2]] = 1; next;)
-  table=??(lr_in_lookup_neighbor), priority=120  , match=(inport == "DR-S3" && (nd_na || nd_ns) && !is_chassis_resident("cr-DR-S3")), action=(reg9[[2]] = 1; next;)
+  table=??(lr_in_lookup_neighbor), priority=120  , match=(inport == "DR-S1" && (nd_na || nd_ns) && eth.mcast && !is_chassis_resident("cr-DR-S1")), action=(reg9[[2]] = 1; next;)
+  table=??(lr_in_lookup_neighbor), priority=120  , match=(inport == "DR-S2" && (nd_na || nd_ns) && eth.mcast && !is_chassis_resident("cr-DR-S2")), action=(reg9[[2]] = 1; next;)
+  table=??(lr_in_lookup_neighbor), priority=120  , match=(inport == "DR-S3" && (nd_na || nd_ns) && eth.mcast && !is_chassis_resident("cr-DR-S3")), action=(reg9[[2]] = 1; next;)
 ])
 # Check the flows in lr_in_gw_redirect stage
 AT_CHECK([grep lr_in_gw_redirect lrflows | grep cr-DR | ovn_strip_lflows], [0], [dnl